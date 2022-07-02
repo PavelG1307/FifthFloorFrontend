b81de890f4f1b694c65131ba0551f6424955b17e
PavelG1307 <julu13@yandex.ru>
2022-07-02 10:30:56 +0300

    developing modules

--- assets/js/scripts_index.js
+++ assets/js/scripts_index.js
@@ -44,42 +44,44 @@ function PutOnGuard(event){
 }
 
 function HandleMessage(request) {
-    const main_btn = document.getElementById('mb').classList
-    if (request["active"]) {
-      main_btn.value = 'main_btn active'
-    } else {
-      main_btn.value = 'main_btn yellow'
-    }
-    document.getElementById("time").innerHTML = IntTimeToStr(request["time"]);
-    document.getElementById("battery").innerHTML = BatteryCharge(request["battery"]);
-    if (request > 0) {
-        stat.light = true
-        document.getElementById('on_btn').classList.value = 'active'
-        document.getElementById('off_btn').classList.value = ''
-    } else {
-        stat.light = false
-        document.getElementById('on_btn').classList.value = ''
-        document.getElementById('off_btn').classList.value = 'active'
-    }
-  
-    const alarm = document.getElementById('alarm').classList
-    if (Object.keys(request.rings).length > 0){
-        alarm.add('active')
-    } else {
-        alarm.remove('active')
-    }
-  
-    const speaker = document.getElementById('speaker').classList
-    if (request.speaker > 0) {
-        speaker.add('active')
-    } else {
-        speaker.remove('active')
-    }
-    const sensors = document.getElementById('sensors').classList
-    if (request.guard){
-        sensors.value = 'btn active';
-    } else {
-        sensors.value = 'btn yellow'
+    if request["type"] == "status"{
+        const main_btn = document.getElementById('mb').classList
+        if (request["active"]) {
+        main_btn.value = 'main_btn active'
+        } else {
+        main_btn.value = 'main_btn yellow'
+        }
+        document.getElementById("time").innerHTML = IntTimeToStr(request["time"]);
+        document.getElementById("battery").innerHTML = BatteryCharge(request["battery"]);
+        if (request > 0) {
+            stat.light = true
+            document.getElementById('on_btn').classList.value = 'active'
+            document.getElementById('off_btn').classList.value = ''
+        } else {
+            stat.light = false
+            document.getElementById('on_btn').classList.value = ''
+            document.getElementById('off_btn').classList.value = 'active'
+        }
+    
+        const alarm = document.getElementById('alarm').classList
+        if (Object.keys(request.rings).length > 0){
+            alarm.add('active')
+        } else {
+            alarm.remove('active')
+        }
+    
+        const speaker = document.getElementById('speaker').classList
+        if (request.speaker > 0) {
+            speaker.add('active')
+        } else {
+            speaker.remove('active')
+        }
+        const sensors = document.getElementById('sensors').classList
+        if (request.guard){
+            sensors.value = 'btn active';
+        } else {
+            sensors.value = 'btn yellow'
+        }
     }
 };
 
