3995ef29e2b63122d300667c10ede5fb7493fc96
PavelG1307 <pavel@iMac-Pavel.local>
2022-07-03 23:11:13 +0300

    debug

--- assets/js/scripts_modules_settings.js
+++ assets/js/scripts_modules_settings.js
@@ -6,7 +6,7 @@ function HandleMessage(request) {
     for (const key in request.modules){
         if (request.modules[key].id_module == id) {
             document.querySelector('#name').setAttribute('placeholder', request.modules[key].name)
-            document.querySelector('#room').setAttribute('placeholder', request.modules[key].room)
+            document.querySelector('#room').setAttribute('placeholder', request.modules[key].location)
             break
         }
     }
