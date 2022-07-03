fe868e13cea1cc668ed3bcb96be9c56ede0e48d5
PavelG1307 <pavel@iMac-Pavel.local>
2022-07-03 23:49:16 +0300

    debug

--- assets/js/scripts_modules_settings.js
+++ assets/js/scripts_modules_settings.js
@@ -2,14 +2,11 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
 
-const el_name = document.getElementById('name')
-const el_loc = document.getElementById('room')
-
 function HandleMessage(request) {
     for (const key in request.modules){
         if (request.modules[key].id_module == id) {
             document.getElementById('name').setAttribute('value', request.modules[key].name)
-            el_loc.setAttribute('value', request.modules[key].location)
+            document.getElementById('room').setAttribute('value', request.modules[key].location)
             break
         }
     }
@@ -20,8 +17,8 @@ function save() {
     wsApp.doSend({
         type: 'UPDATE MODULE',
         module: id,
-        name: el_name.value,
-        location: el_loc.value
+        name: document.getElementById('name').value,
+        location: ocument.getElementById('room').value
     })
 }
 
