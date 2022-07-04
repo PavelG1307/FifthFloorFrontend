8bde724075d08c4af2c2849431795ecff545c39b
PavelG1307 <pavel@iMac-Pavel.local>
2022-07-04 16:17:05 +0300

    debug

--- assets/js/scripts_alarm_edit.js
+++ assets/js/scripts_alarm_edit.js
@@ -5,8 +5,7 @@ const id = urlParams.get('id')
 function HandleMessage(req) {
     for (key in req.rings) {
         console.log('.')
-        console.log(req.ring[key].id)
-        if (req.ring[key].id == id) {
+        if (req.rings[key].id == id) {
             console.log('sdfsdc')
             const ring = req.ring[key]
             document.querySelector('#time').value = IntTimeToStr(ring.time)
