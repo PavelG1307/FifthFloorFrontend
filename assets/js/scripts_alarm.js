26477426180796c83f7df309056648c24f6ef105
PavelG1307 <pavel@iMac-Pavel.local>
2022-07-04 14:52:27 +0300

    debug

--- assets/js/scripts_alarm.js
+++ assets/js/scripts_alarm.js
@@ -4,9 +4,24 @@ function HandleMessage(request){
         ring = request.rings[key]
         const {id,time, active} = ring
         time_str = IntTimeToStr(time)
-        const time_left = 25
+        time_left = timeLeft(time)
         ring_el = `<div class="alarm" id="${id}"><div class="alarm_container" onclick="document.location.href = './edit.html?id=${id}'"><div class="circle"></div><div class="time">${time_str}</div><div class="remains"><p>До будильника</p><p>${time_left} мин.</p></div></div></div>`
         container.insertAdjacentHTML('beforeend', ring_el)
+    }
+}
+
+function timeLeft(time) {
+    const date = new Date()
+    const time_now = date.getHours()*60 + date.getMinutes()
+    if (time_now > time) {
+        const time_left = time_now - time
+    } else {
+        const time_left = time - time_now + 1440
+    }
+    if (time_left < 60) {
+        return `${time_left} мин.`
+    } else {
+        return `${Math.round(time_left/60)} ч. ${time_left%60} мин.`
     }
 }
 
