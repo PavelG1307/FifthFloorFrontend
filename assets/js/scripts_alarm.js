d47c6e8a1078c010973a0b0acc2bd20a7e723122
PavelG1307 <pavel@iMac-Pavel.local>
2022-07-04 14:34:56 +0300

    debug

---
+++ assets/js/scripts_alarm.js
@@ -0,0 +1,11 @@
+function HandleMessage(request){
+    const container = document.getElementsByClassName('container')
+    for (key in request.rings) {
+        ring = request.rings[key]
+        const {id,time, active} = ring
+        const time_left = 25
+        ring_el = `<div class="alarm_container" id="${id}" onclick="document.location.href = './edit.html?id=${id}'"><div class="circle"></div><div class="time">${time}</div><div class="remains"><p>До будильника</p><p>${time_left} мин.</p></div></div>`
+        container.insertAdjacentHTML('beforeend', ring_el)
+    }
+}
+
