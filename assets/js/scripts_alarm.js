c1905c93ee46b38bbe477217eaecad6393d01cc7
PavelG1307 <pavel@iMac-Pavel.local>
2022-07-04 14:54:51 +0300

    debug

--- assets/js/scripts_alarm.js
+++ assets/js/scripts_alarm.js
@@ -5,7 +5,7 @@ function HandleMessage(request){
         const {id,time, active} = ring
         const time_str = IntTimeToStr(time)
         const time_left = timeLeft(time)
-        const ring_el = `<div class="alarm" id="${id}"><div class="alarm_container" onclick="document.location.href = './edit.html?id=${id}'"><div class="circle"></div><div class="time">${time_str}</div><div class="remains"><p>До будильника</p><p>${time_left} мин.</p></div></div></div>`
+        const ring_el = `<div class="alarm" id="${id}"><div class="alarm_container" onclick="document.location.href = './edit.html?id=${id}'"><div class="circle"></div><div class="time">${time_str}</div><div class="remains"><p>До будильника</p><p>${time_left}</p></div></div></div>`
         container.insertAdjacentHTML('beforeend', ring_el)
     }
 }
@@ -13,10 +13,11 @@ function HandleMessage(request){
 function timeLeft(time) {
     const date = new Date()
     const time_now = date.getHours()*60 + date.getMinutes()
+    let time_left
     if (time_now > time) {
-        const time_left = time_now - time
+        time_left = time_now - time
     } else {
-        const time_left = time - time_now + 1440
+        time_left = time - time_now + 1440
     }
     if (time_left < 60) {
         return `${time_left} мин.`
