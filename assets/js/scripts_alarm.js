580d02180dabb28ba884df02dd5cbd0d12ea4a56
PavelG1307 <pavel@iMac-Pavel.local>
2022-07-04 14:39:11 +0300

    debug

--- assets/js/scripts_alarm.js
+++ assets/js/scripts_alarm.js
@@ -4,7 +4,7 @@ function HandleMessage(request){
         ring = request.rings[key]
         const {id,time, active} = ring
         const time_left = 25
-        ring_el = `<div class="alarm_container" id="${id}" onclick="document.location.href = './edit.html?id=${id}'"><div class="circle"></div><div class="time">${time}</div><div class="remains"><p>До будильника</p><p>${time_left} мин.</p></div></div>`
+        ring_el = `<div class="alarm" id="${id}"><div class="alarm_container" onclick="document.location.href = './edit.html?id=${id}'"><div class="circle"></div><div class="time">${time}</div><div class="remains"><p>До будильника</p><p>${time_left} мин.</p></div></div></div>`
         container.insertAdjacentHTML('beforeend', ring_el)
     }
 }
