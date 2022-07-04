b4ed6ffb5f8d559732d3619e5596fe7ecfdea374
PavelG1307 <pavel@iMac-Pavel.local>
2022-07-04 18:05:04 +0300

    debug

--- assets/js/scripts_alarm.js
+++ assets/js/scripts_alarm.js
@@ -7,7 +7,7 @@ function HandleMessage(request){
         const time_str = IntTimeToStr(time)
         const time_left = timeLeft(time)
         const class_active = active?" active":""
-        const ring_el = `<div class="alarm${class_active}" id="${id}"><div class="alarm_container" onclick="onClickAlarm(id)" oncontextmenu="return long_press(event, 'edit.html?id=${id}')"><div class="circle"></div><div class="time">${time_str}</div><div class="remains"><p>До будильника</p><p>${time_left}</p></div></div></div>`
+        const ring_el = `<div class="alarm${class_active}" id="${id}"><div class="alarm_container" onclick="onClickAlarm(id)" oncontextmenu="return long_press(event, 'alarm/edit.html?id=${id}')"><div class="circle"></div><div class="time">${time_str}</div><div class="remains"><p>До будильника</p><p>${time_left}</p></div></div></div>`
         container.insertAdjacentHTML('beforeend', ring_el)
     }
 }
