ce48736951dc6bab9336afaec1e932680c30ce09
Pavel_prod <pavel.g@zyfrovod.ru>
2022-09-04 00:06:19 +0300

    время, слушатель enter

--- assets/js/alarm_edit.js
+++ assets/js/alarm_edit.js
@@ -3,7 +3,12 @@ const urlParams = new URLSearchParams(queryString);
 let id = urlParams.get('id')
 
 function switchParam(id) {
-  document.getElementById(id).classList.toggle('active')
+  const button = document.getElementById(id) 
+  button.classList.toggle('active')
+  console.log(button)
+  if (id === 'mode') {
+    button.innerText = button.classList.contains('active') ? 'Выключить' : 'Включить'
+  }
 }
 
 async function getRings() {
@@ -19,6 +24,8 @@ async function setData(data) {
   }
   if (id === 'new'){
     id = data
+    const date = new Date()
+    document.querySelector('#time').value = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2)
     return
   }
   if (data[0].active) document.querySelector('#mode').classList.add('active')
