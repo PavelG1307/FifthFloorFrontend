65910d3f884553d1cc2a16aab217d516bdd45c20
Pavel_prod <pavel.g@zyfrovod.ru>
2022-09-05 02:04:41 +0300

    test

--- assets/js/modules_settings.js
+++ assets/js/modules_settings.js
@@ -2,8 +2,43 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const id = urlParams.get('id')
 
+
 let actions = []
 let value = 0
+
+async function getActionEl(actions, i, placeholder) {
+  let action = `
+    <div class="if">
+      <div class="row">
+          <select id="type">
+              <option value="0">></option>
+              <option value="1"><</option>
+          </select>
+          <input type="text" id="value_${i}" placeholder="${placeholder}">
+          <button id="btn" onclick="deleteTask(id)">x</button>
+      </div>
+    <select id="action">`
+  for (const i in actions) {
+    const action_true = actions[i].action_true + ' «' + actions[i].name + '»'
+    const action_false = actions[i].action_false + ' «' + actions[i].name + '»'
+    action += `<option value="${2*i}"> ${action_true} </option>`
+    action += `<option value="${2*i + 1}"> ${action_false} </option>`
+  }
+  action += `</select></div>`
+  return action
+}
+
+let count_actions = 0
+async function addTask() {
+  const button = document.getElementById('btn_add')
+  const node = await getActionEl(actions, count_actions, value)
+  button.insertAdjacentHTML('beforebegin', node);
+  count_actions++
+  if (count_actions > 1) {
+    button.remove()
+  }
+}
+
 
 async function getModule() {
   const res = await axios({
@@ -55,35 +90,3 @@ function del() {
   })
 }
 
-async function getActionEl(actions, i, placeholder) {
-  let action = `
-    <div class="if">
-      <div class="row">
-          <select id="type">
-              <option value="0">></option>
-              <option value="1"><</option>
-          </select>
-          <input type="text" id="value_${i}" placeholder="${placeholder}">
-          <button id="btn" onclick="deleteTask(id)">x</button>
-      </div>
-    <select id="action">`
-  for (const i in actions) {
-    const action_true = actions[i].action_true + ' «' + actions[i].name + '»'
-    const action_false = actions[i].action_false + ' «' + actions[i].name + '»'
-    action += `<option value="${2*i}"> ${action_true} </option>`
-    action += `<option value="${2*i + 1}"> ${action_false} </option>`
-  }
-  action += `</select></div>`
-  return action
-}
-
-let count_actions = 0
-async function addTask() {
-  const button = document.getElementById('btn_add')
-  const node = await getActionEl(actions, count_actions, value)
-  button.insertAdjacentHTML('beforebegin', node);
-  count_actions++
-  if (count_actions > 1) {
-    button.remove()
-  }
-}
