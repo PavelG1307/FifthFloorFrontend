f81f1b7fcbe17ee93fb67429bd624af9ce53ae56
Pavel_prod <julu13@yandex.ru>
2022-10-07 17:36:43 +0300

    fdsfsdf

--- speaker/scripts.js
+++ speaker/scripts.js
@@ -1,36 +1,67 @@
 (function () {
   
-    // Create variable for setTimeout
-    var delay;
+  // Create variable for setTimeout
+  var delay;
+  
+  // Set number of milliseconds for longpress
+  var longpress = 300;
+  
+  var listItems = document.getElementsByClassName('list-item');
+  var listItem;
+  
+  for (var i = 0, j = listItems.length; i < j; i++) {
+    listItem = listItems[i];
     
-    // Set number of milliseconds for longpress
-    var longpress = 300;
-    
-    var listItems = document.getElementsByClassName('list-item');
-    var listItem;
-    
-    for (var i = 0, j = listItems.length; i < j; i++) {
-      listItem = listItems[i];
-      
-      listItem.addEventListener('touchend', function (e) {
+    listItem.addEventListener('touchstart', function (e) {
         var _this = this;
         delay = setTimeout(check, longpress);
         
         function check() {
-            _this.innerText = 'sdjflksajflkf';
+            _this.classList.add('is-selected');
         }
         
       }, true);
+    listItem.addEventListener('touchend', function (e) {
+      var _this = this;
+      delay = setTimeout(check, longpress);
       
-      listItem.addEventListener('touchstart', function (e) {
-        // On mouse up, we know it is no longer a longpress
-        clearTimeout(delay);
-      });
+      function check() {
+          _this.classList.add('is-selected');
+      }
       
-      listItem.addEventListener('touchend', function (e) {
-        clearTimeout(delay);
-      });
-      
-    }
+    }, true);
     
-  }());
+    listItem.addEventListener('touchstart', function (e) {
+      // On mouse up, we know it is no longer a longpress
+      clearTimeout(delay);
+    });
+    
+    listItem.addEventListener('touchend', function (e) {
+      clearTimeout(delay);
+    });
+
+
+
+
+    listItem.addEventListener('mousedown', function (e) {
+      var _this = this;
+      delay = setTimeout(check, longpress);
+      
+      function check() {
+          _this.classList.add('is-selected');
+      }
+      
+    }, true);
+    
+    listItem.addEventListener('mouseup', function (e) {
+      // On mouse up, we know it is no longer a longpress
+      clearTimeout(delay);
+    });
+    
+    listItem.addEventListener('mouseout', function (e) {
+      clearTimeout(delay);
+    });
+    
+  }
+  
+}());
