82f27752e4fc95a705231b3cf24b788d54412685
Pavel_prod <julu13@yandex.ru>
2022-10-07 17:00:02 +0300

    тест драг энд жроп

--- speaker/scripts.js
+++ speaker/scripts.js
@@ -1,21 +1,53 @@
-let onlongtouch; 
-let timer;
-let touchduration = 200; //length of time we want the user to touch before we do something
+// let onlongtouch; 
+// let timer;
+// let touchduration = 200; //length of time we want the user to touch before we do something
 
 
-document.getElementById('btnn').addEventListener("mouseup", ()=>touchend());
-document.getElementById('btnn').addEventListener("mousedown", ()=>touchstart());
+document.getElementById('btnn')
+// document.getElementById('btnn').addEventListener("mousedown", ()=>touchstart());
 
-function touchstart() {
-  timer = setTimeout(onlongtouch, touchduration); 
+// function touchstart() {
+//   timer = setTimeout(onlongtouch, touchduration); 
+// }
+
+// function touchend() {
+//     //stops short touches from firing the event
+//     if (timer)
+//         clearTimeout(timer); // clearTimeout, not cleartimeout..
+// }
+
+// onlongtouch = function() {
+//   document.getElementById('btnn').innerText = 'SUCCESS'
+// };
+
+
+
+const button = document.getElementById('btnn');
+let startpos = -1
+button.onmousedown = function (ev) {
+    if (startpos < 0) {
+        startpos = button.offsetWidth
+    }
+    // button.style.position = 'absolute'
+  moveAt(ev);
+  document.body.appendChild(button);
+  button.style.zIndex = 1000;
+  function moveAt(e) {
+    const marg = 2 * e.pageX - button.offsetWidth / 2 - startpos
+    if (marg<100 && marg>-100) {
+        button.style.marginLeft = marg + 'px';
+    }
+    // button.style.top = e.pageY - button.offsetHeight / 2 + 'px';
+  }
+  document.onmousemove = function(ev) {
+    moveAt(ev);
+  }
+  
 }
 
-function touchend() {
-    //stops short touches from firing the event
-    if (timer)
-        clearTimeout(timer); // clearTimeout, not cleartimeout..
+button.onmouseup = function() {
+  document.onmousemove = null;
+  document.onmousedown = null;
+  startpos = -1
+  button.style.marginLeft = 0
 }
-
-onlongtouch = function() {
-  document.getElementById('btnn').innerText = 'SUCCESS'
-};
